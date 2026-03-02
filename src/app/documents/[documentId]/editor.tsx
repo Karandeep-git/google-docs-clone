5e3b4de44b49e12f4357ec5990baf4764d6d0a20
Karandeep <softwaredevkaran@gmail.com>
2026-03-02 22:56:02 +0530

    Heading updated

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -4,6 +4,7 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import { TaskList } from '@tiptap/extension-task-list'
 import { TaskItem } from '@tiptap/extension-task-item'
 import StarterKit from "@tiptap/starter-kit"
+import { Heading } from "@tiptap/extension-heading";
 import { Table } from "@tiptap/extension-table";
 import { TableRow } from "@tiptap/extension-table-row";
 import { TableCell } from "@tiptap/extension-table-cell";
@@ -80,6 +81,10 @@ export const Editor = ({ initialContent }: EditorProps) => {
       liveblocks,
       StarterKit.configure({
         history: false,
+        heading: false,
+      }),
+      Heading.configure({
+        levels: [1, 2, 3, 4, 5, 6],
       }),
       TaskList,
       TaskItem.configure({
